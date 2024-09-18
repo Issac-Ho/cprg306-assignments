bfeaee8af372f62371c6a81404add3ae5a18b992
Issac-Ho <issac.ho@edu.sait.ca>
2024-09-18 11:40:53 -0600

    week 5 assignments

---
+++ app/week-5/page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NewItem from './new-item';
+
+const Page = () => {
+  return (
+    <main class="flex justify-center w-full">
+      <NewItem />
+    </main>
+  );
+};
+
+export default Page;
+
+      
